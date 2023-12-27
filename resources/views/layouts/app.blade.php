7786e8f6d7ab42904d499476ec03aefb9dc6b6cf
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-12-27 17:36:04 +0800

    Index order

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,90 +1,63 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        
-        <title>UMPSA OnPrint</title>
-        
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-        
-        <!-- Styles -->
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.css">
-        
-        <!-- Icons -->
-        <link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
-        
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+<html lang="en">
 
-        
-        @livewireStyles
-    </head>
-    <body class="font-sans antialiased">
-        <x-banner />
+<head>
 
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
-        
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-        
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>UMPSA OnPrint</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css"
+        integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous">
+
+    <link href="https://bootstrap-ecommerce.com/bootstrap-ecommerce-html/images/favicon.ico" rel="shortcut icon"
+        type="image/x-icon">
+
+</head>
+
+<body>
+
+    @livewire('navigation-menu')
+
+    {{ $slot }}
+
+    <br>
+    <br>
+
+    <footer class="footer pt-4 pb-5" id="footer">
+        <div class="container">
+
+            <span class="text-muted float-left">
+                <p id="copyright">&copy; 2023 Section 01B</p>
+            </span>
+
+            <span class="float-right">
+                <p id="footerInfo"> Zhafran Zainal </p>
+            </span>
+
         </div>
+    </footer>
 
-        @stack('modals')
-        
-        @livewireScripts
-        
-        @stack('scripts')
-        
-        <script src="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.js"></script>
-        
-        @if (session()->has('success')) 
-        <script>
-            var notyf = new Notyf({dismissible: true})
-            notyf.success('{{ session('success') }}')
-        </script> 
-        @endif
-        
-        <script>
-            /* Simple Alpine Image Viewer */
-            document.addEventListener('alpine:init', () => {
-                Alpine.data('imageViewer', (src = '') => {
-                    return {
-                        imageUrl: src,
-        
-                        refreshUrl() {
-                            this.imageUrl = this.$el.getAttribute("image-url")
-                        },
-        
-                        fileChosen(event) {
-                            this.fileToDataUrl(event, src => this.imageUrl = src)
-                        },
-        
-                        fileToDataUrl(event, callback) {
-                            if (! event.target.files.length) return
-        
-                            let file = event.target.files[0],
-                                reader = new FileReader()
-        
-                            reader.readAsDataURL(file)
-                            reader.onload = e => callback(e.target.result)
-                        },
-                    }
-                })
-            })
-        </script>
-    </body>
+</body>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+</script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+    integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+</script>
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+    integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+</script>
+
+<script src="js/script.js"></script>
+
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,144 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Online Printing System (OnPrint)</title>
-
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
-        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-
-    <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
-
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css"
-        integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous">
-
-    <link href="https://bootstrap-ecommerce.com/bootstrap-ecommerce-html/images/favicon.ico" rel="shortcut icon"
-        type="image/x-icon">
-
-</head>
-
-<body>
-
-    <div class="top-header bg-primary">
-        <div class="container">
-            <div class="row">
-
-                <div class="col-md-7 mt-2">
-                    <ul>
-                        <li><a href="{{ route('dashboard') }}">Home</a></li>
-                        <li><a href="{{ route('orders.index') }}">Order</a></li>
-                        <li><a href="(4) Payment/payment.php">Payment</a></li>
-                        <li><a href="{{ route('deliveries.index') }}">Delivery</a></li>
-                        <li><a href="(6) Sales and Admin/admin.html">Sales and Admin</a></li>
-                    </ul>
-                </div>
-
-                @if (Route::has('login'))
-                    <div class="col-md-5 mt-2 ">
-                        @auth
-                            <h6><a href="{{ url('/dashboard') }}" style="color: white;">Dashboard</a></h6>
-                        @else
-                            <h6><a href="{{ route('login') }}" style="color: white;">Login</a></h6>
-                        @endauth
-                    </div>
-                @endif
-
-            </div>
-        </div>
-    </div>
-
-    <div class="container mt-4" id="top-logo">
-        <div class="row">
-
-            <div class="col-md-2" style="right: 5%;">
-                <nav class="navbar navbar-light">
-                    <a class="navbar-brand" href="#">
-                        <img src="{{ asset('assets/images/logo.png') }}" width="auto" height="50">
-                    </a>
-                </nav>
-            </div>
-
-            <div class="col-md-7" id="searchbar">
-                <div class="input-group mt-3">
-                    <input type="text" class="form-control" placeholder="Search" aria-label="Search"
-                        aria-describedby="button-addon2">
-                    <div class="input-group-append">
-                        <button class="btn btn-primary" type="button" id="button-addon2"><i
-                                class="fas fa-search"></i></button>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-3 mt-2" id="icons">
-
-                <a href="{{ route('profile.show') }}">
-                    <div class="circle float-right" title="Manage Profile">
-                        <i class="fas fa-user text-primary"></i>
-                    </div>
-                </a>
-
-                <a href="(3) Printing Order/cart.html">
-                    <div class="circle float-right mr-2">
-                        <i class="fas fa-shopping-cart text-primary"></i>
-                        <sup><span class="badge badge-danger" style="width: 23px;">0</span></sup>
-                    </div>
-                </a>
-
-            </div>
-
-        </div>
-    </div>
-    <hr>
-
-    <div class="container" style="height: 30px;">
-        <nav class="navbar navbar-expand-lg navbar-light" style="height: 30px;">
-
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse pl-4" id="navbarNav">
-                <ul class="navbar-nav">
-
-                    <li class="nav-item dropdown">
-
-                        <a class="nav-link dropdown" data-toggle="dropdown" href="#" role="button"
-                            aria-haspopup="true" aria-expanded="false"> <span> ≡ All Category</span></a>
-
-                        <div class="dropdown-menu">
-                            <a class="dropdown-item" href="#">Comb Bind Notebook</a>
-                            <a class="dropdown-item" href="#">Tape Bind Notebook</a>
-                            <a class="dropdown-item" href="#">Certificate Printing</a>
-                            <a class="dropdown-item" href="#">Thesis Hard Cover</a>
-                            <a class="dropdown-item" href="#">Poster</a>
-                        </div>
-
-                    </li>
-
-                    <li class="nav-item dropdown">
-
-                        <a class="nav-link dropdown" data-toggle="dropdown" href="#" role="button"
-                            aria-haspopup="true" aria-expanded="false">
-                            Campus
-                        </a>
-
-                        <div class="dropdown-menu">
-                            <a class="dropdown-item" href="(3) Printing Order/campus-pekan.php">UMP Pekan</a>
-                            <a class="dropdown-item" href="(3) Printing Order/campus-gambang.php">UMP Gambang</a>
-                        </div>
-
-                    </li>
-
-                </ul>
-            </div>
-        </nav>
-
-    </div>
-    <hr>
+<x-app-layout>
 
     <div class="container">
         <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
@@ -146,9 +6,8 @@
             <div class="carousel-inner">
 
                 <div class="carousel-item active">
-                    <img class="d-block w-100" src="{{ asset('assets/images/comb_bind_notebook.jpg') }}"
-                        alt="First slide" class="img-fluid">
-
+                    <img class="d-block w-100" src="{{ asset('assets/images/comb_bind_notebook.jpg') }}" alt="First slide"
+                        class="img-fluid">
                 </div>
 
                 <div class="carousel-item">
@@ -177,10 +36,12 @@
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="sr-only">Previous</span>
             </a>
+
             <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="sr-only">Next</span>
             </a>
+
         </div>
     </div>
 
@@ -232,8 +93,8 @@
 
                     <div class="card-body">
 
-                        <p class="card-text"><a href="inner-productpage.html" class="text-dark">Comb Bind
-                                Notebook</a>
+                        <p class="card-text"><a href="inner-productpage.html" class="text-dark">
+                                Comb Bind Notebook</a>
                         </p>
 
                         <div class="ratings">
@@ -339,47 +200,4 @@
 
     </div>
 
-    <br>
-    <br>
-
-    <footer>
-        <br><br>
-        <div class="container">
-            <div class="row">
-
-            </div>
-        </div>
-    </footer>
-
-    <footer class="footer pt-5 pb-5" id="footer">
-        <div class="container">
-
-            <span class="text-muted float-left">
-                <p id="copyright">&copy; 2022 Section 02B GP5</p>
-            </span>
-
-            <span class="float-right">
-                <p id="footerInfo"> Zhafran | Anishah | Amalin | Hamizah</p>
-            </span>
-
-        </div>
-    </footer>
-
-</body>
-
-
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
-</script>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
-    integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
-</script>
-
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
-    integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
-</script>
-
-<script src="js/script.js"></script>
-
-</html>
+</x-app-layout>
