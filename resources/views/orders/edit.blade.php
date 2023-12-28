7aba8897965061613d002204d6203052cbddf78a
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-12-28 14:23:02 +0800

    Update edit.blade.php

--- resources/views/orders/edit.blade.php
+++ resources/views/orders/edit.blade.php
@@ -1,49 +1,35 @@
 <x-app-layout>
 
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            @lang('crud.orders.edit_title')
-        </h2>
-    </x-slot>
+    <div class="container" id="card">
+        <div class="row">
+            <div class="col">
+                <div class="card p-4" id="cards">
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-
-            <x-partials.card>
-
-                <x-slot name="title">
-                    <a href="{{ route('orders.index') }}" class="mr-4">
-                        <i class="mr-1 icon ion-md-arrow-back"></i>
+                    <a href="{{ route('orders.index-campus') }}" class="button">
+                        @lang('crud.common.back')
                     </a>
-                </x-slot>
 
-                <x-form method="PUT" action="{{ route('orders.update', $order) }}" class="mt-4">
+                    <br>
+                    <h5 class="text-dark pl-4">@lang('crud.orders.edit_title')</h5>
 
-                    @include('orders.form-inputs')
+                    <x-form method="PUT" action="{{ route('orders.update', $order) }}" class="mt-4">
 
-                    <div class="mt-10">
+                        @include('orders.form-inputs')
 
-                        <a href="{{ route('orders.index') }}" class="button">
-                            <i class="mr-1 icon ion-md-return-left text-primary"></i>
-                            @lang('crud.common.back')
-                        </a>
+                        <div class="mt-10">
+                            <button type="submit" class="button button-primary float-right">
+                                @lang('crud.common.update')
+                            </button>
+                        </div>
 
-                        <a href="{{ route('orders.create') }}" class="button">
-                            <i class="mr-1 icon ion-md-add text-primary"></i>
-                            @lang('crud.common.create')
-                        </a>
+                        @foreach ($errors->all() as $error)
+                            <span class="text-danger">{{ $error }}</span><br>
+                        @endforeach
 
-                        <button type="submit" class="button button-primary float-right">
-                            <i class="mr-1 icon ion-md-save"></i>
-                            @lang('crud.common.update')
-                        </button>
+                    </x-form>
 
-                    </div>
-
-                </x-form>
-
-            </x-partials.card>
-
+                </div>
+            </div>
         </div>
     </div>
 
