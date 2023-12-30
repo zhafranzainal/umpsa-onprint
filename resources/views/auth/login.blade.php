f952c57c35f55f0170f7471a28e8757f86202197
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-12-30 17:52:10 +0800

    Add register link

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -54,6 +54,12 @@
                     </x-button>
                 </div>
 
+                <br>
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    href="{{ route('register') }}">
+                    {{ __('New user? Register here') }}
+                </a>
+
             </form>
 
         </div>
